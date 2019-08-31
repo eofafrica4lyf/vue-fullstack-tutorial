8efcc6dc0be06498852bb065155a3ad93b9d6950
Emmanuel Aboderin <aboderinemmanuel@gmail.com>
2019-08-31 16:40:46 +0100

    name build folder as 'public' in the server folder, and point the static folder on the server root file to it(index.html).

--- client/vue.config.js
+++ client/vue.config.js
@@ -1,6 +1,8 @@
 const path = require('path');
 
 module.exports = {
+  // build our files in the server directory
+  outputDir: path.resolve(__dirname, '../server/public'),
   devServer: {
     proxy: {
       '/api': {
--- server/index.js
+++ server/index.js
@@ -12,6 +12,17 @@ const posts = require('./routes/api/post.js')
 
 app.use('/api/posts', posts);
 
+
+//Handle production. Make sure this is below the route just above
+if(process.env.NODE_ENV === 'production'){
+  //Static folder
+  app.use(express.static(__dirname + '/public'));
+
+  //Handle SPA
+  app.get(/.*/, (req,res) => res.sendFile(__dirname + '/public/index.html'))
+}
+
+
 const port = process.env.PORT || 5000;
 
 app.listen(port, () => console.log(`Server started on port ${port}`));
